5e2d8d4d415b3f6a5234dce04618b13f785de33d
Carver <kerenketepela@gmail.com>
2022-01-28 16:30:56 +0300

    lesson 1

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "Enter The First Number: ";
+    cin >> a;
+    cout << "Enter The Second Number: ";
+    cin >> b;
+    cout << "Enter The third Number: ";
+    cin >> c;
+
+    int product;
+    product = a * b * c;
+
+    cout << "The sum of the two numbers is: " << product << endl;
+
+    return 0;
+}
