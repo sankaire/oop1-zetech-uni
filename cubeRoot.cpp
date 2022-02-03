ce9d027412e61489151fd4afaf622a395bde99fb
Carver <kerenketepela@gmail.com>
2022-02-03 11:59:02 +0300

    cuberoot of a number

---
+++ cubeRoot.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter an Interger: ";
+    cin >> num;
+
+    cout << "Cube root: " << std::cbrt(num) << endl;
+}
