75d4d836cb43d5773020339d1c3e1d37d288cb93
Carver <kerenketepela@gmail.com>
2022-02-03 12:00:56 +0300

    find power of a number

---
+++ powerOfNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int base, exponent, result;
+    cout << "Enter value to be powered: ";
+    cin >> base;
+    cout << "Enter value to power the first number: ";
+    cin >> exponent;
+    result = pow(base, exponent);
+    cout << "Result=" << result << endl;
+    return 0;
+}
