7cce41dbf23577d801cb37902ea3f710c2d54dd3
Carver <kerenketepela@gmail.com>
2022-02-04 16:32:16 +0300

    cascade

--- sumofThreeNo.cpp
+++ sumofThreeNo.cpp
@@ -2,31 +2,18 @@
 
 #include <iostream>
 using namespace std;
-/* multiple line */
-// single line comentss
 
 int main()
 {
-    int a, b, c;
-    // const float pie = 3.142;
+    int a, b, c, sum, product;
 
-    // float r = pie * a;
-    // cout << r << endl;
-
-    cout << "Enter The First Number: ";
-    cin >> a;
-    cout << "Enter The Second Number: ";
-    cin >> b;
-    cout << "Enter The third Number: ";
-    cin >> c;
-
-    int sum;
-    int product;
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
     sum = a + b + c;
     product = a * b * c;
 
-    cout << "The sum of the three numbers is: " << sum << endl;
-    cout << "The product of the three numbers is: " << product << endl;
+    cout << "The sum of the three numbers is: " << sum << endl
+         << "The product of the three numbers is: " << product << endl;
 
     return 0;
 }
